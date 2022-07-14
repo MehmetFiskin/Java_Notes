70d679cf584b76d936606fff5ad7680d90f86271
mehmetfiskin <malifiskin@gmail.com>
2022-07-14 10:14:08 -0400

    about forloop

--- src/day4_practice/Q04_ForLoop.java
+++ src/day4_practice/Q04_ForLoop.java
@@ -12,7 +12,16 @@ public class Q04_ForLoop {
      52
      39
      26
-     13       */
+     13  */
+        int toplam = 0;
+        System.out.println("istenen sonuc :");
+        for (int i = 100; i >=0 ; i--) {
+            if(i%13==0){
+                System.out.println(i);
+                toplam +=i;
+            }
+        }
+        System.out.println("toplam = " + toplam);
 
 
 
