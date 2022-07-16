58439a59506c61c96cc68cf9ea9158e0e537883c
mehmetfiskin <malifiskin@gmail.com>
2022-07-15 22:16:27 -0400

    about forloop

---
+++ src/day5_Practice/Q01_ForLoop.java
@@ -0,0 +1,34 @@
+package day5_Practice;
+
+import java.util.Scanner;
+
+public class Q01_ForLoop {
+    public static void main(String[] args) {
+        /*Kullanıcıdan bir harf girmesini isteyiniz. Girilen harf sesli ise Sesli harf olduğunu,
+     değilse sessiz harf olduğunu ekrana yazdırsın. Girdiği değer harf değilse yada
+     1 karakterden fazla ise hata mesajı göstersin. (Test girilen harfi büyük yada küçüklüğüne duyarlıdır.)
+     Sesli harfler: a,e,i,o,u
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("lutfen bir harf giriniz :");
+        String harf = scanner.next().toLowerCase();
+        String sesliHarfler = "aeiou";
+        String sessizHarfler = "bcdfghjklmnpqrstxvwyz";
+        if(harf.length()==1){
+            for (int i = 0; i < sesliHarfler.length() ; i++) {
+                if(sesliHarfler.contains(harf)){
+                    System.out.println("sesli harf");
+                    break;
+                }else if(sessizHarfler.contains(harf)){
+                    System.out.println("sessiz harf");
+                    break;
+                }else {
+                    System.out.println("yanlis karakter girdiniz");
+                    break;
+                }
+            }
+        }else System.out.println("tek karakter giriniz");
+
+    }
+}
