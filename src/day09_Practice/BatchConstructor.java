f0416b62ab1a24eb73ba481913b1d6095af6fd06
mehmetfiskin <malifiskin@gmail.com>
2022-07-29 12:57:23 -0400

    about for constructor

---
+++ src/day09_Practice/BatchConstructor.java
@@ -0,0 +1,17 @@
+package day09_Practice;
+
+public class BatchConstructor {
+
+    public static void main(String[] args) {
+
+
+        Batch batch =new Batch();
+
+        System.out.println("batch.str1 = " + batch.str1);
+
+        batch.batch();
+
+    }
+
+
+}
